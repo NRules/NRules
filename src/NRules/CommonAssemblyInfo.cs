6dc0cb76d76f321e2b7d248dfd4c6121033cc146
snikolayev <snikolayev@github.com>
2013-01-08 23:14:21 -0500

    Updated copyright year

--- src/NRules/CommonAssemblyInfo.cs
+++ src/NRules/CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Security;
 
 [assembly: AssemblyCompany("NRules")]
 [assembly: AssemblyProduct("NRules")]
-[assembly: AssemblyCopyright("Copyright 2012 Sergiy Nikolayev. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright 2012-2013 Sergiy Nikolayev. All rights reserved.")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AllowPartiallyTrustedCallers]
