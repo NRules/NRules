9f4ab21b5f0ff1f9010cf7b3e9d582f896616bb5
snikolayev <snikolayev@github.com>
2014-01-01 16:23:15 -0500

    Change copyright year

--- samples/SimpleRulesTest/SimpleRulesTest/Properties/AssemblyInfo.cs
+++ samples/SimpleRulesTest/SimpleRulesTest/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("NRules")]
 [assembly: AssemblyProduct("NRules")]
-[assembly: AssemblyCopyright("Copyright 2012-2013 Sergiy Nikolayev. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright 2012-2014 Sergiy Nikolayev. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
