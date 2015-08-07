12cff124e4bbb42152d916b1d5ef764c6cf72d1b
snikolayev <snikolayev@github.com>
2015-08-06 21:57:33 -0400

    Improve node sharing test

--- src/NRules/Tests/NRules.IntegrationTests/TestRules/TwinRuleOne.cs
+++ src/NRules/Tests/NRules.IntegrationTests/TestRules/TwinRuleOne.cs
@@ -1,4 +1,7 @@
-﻿using NRules.IntegrationTests.TestAssets;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NRules.Fluent.Dsl;
+using NRules.IntegrationTests.TestAssets;
 
 namespace NRules.IntegrationTests.TestRules
 {
@@ -8,10 +11,20 @@ namespace NRules.IntegrationTests.TestRules
         {
             FactType1 fact1 = null;
             FactType2 fact2 = null;
+            IEnumerable<FactType5> group = null;
 
             When()
                 .Match<FactType1>(() => fact1, f => f.TestProperty.StartsWith("Valid"))
-                .Match<FactType2>(() => fact2, f => f.TestProperty.StartsWith("Valid"), f => f.JoinProperty == fact1.TestProperty);
+                .Match<FactType2>(() => fact2, f => f.TestProperty.StartsWith("Valid"), f => f.JoinProperty == fact1.TestProperty)
+                .Not<FactType3>(f => f.TestProperty.StartsWith("Invalid"))
+                .Exists<FactType3>(f => f.TestProperty.StartsWith("Valid"))
+                .Query(() => group, q => q
+                    .Match<FactType5>()
+                    .Where(f => f.TestProperty.StartsWith("Valid"))
+                    .GroupBy(f => f.TestProperty)
+                    .SelectMany(x => x)
+                    .Collect()
+                    .Where(c => c.Any()));
 
             Then()
                 .Do(ctx => Action());
--- src/NRules/Tests/NRules.IntegrationTests/TestRules/TwinRuleTwo.cs
+++ src/NRules/Tests/NRules.IntegrationTests/TestRules/TwinRuleTwo.cs
@@ -1,4 +1,7 @@
-﻿using NRules.IntegrationTests.TestAssets;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NRules.Fluent.Dsl;
+using NRules.IntegrationTests.TestAssets;
 
 namespace NRules.IntegrationTests.TestRules
 {
@@ -6,12 +9,22 @@ namespace NRules.IntegrationTests.TestRules
     {
         public override void Define()
         {
-            FactType1 fact11 = null;
-            FactType2 fact21 = null;
+            FactType1 fact1 = null;
+            FactType2 fact2 = null;
+            IEnumerable<FactType5> group = null;
 
             When()
-                .Match<FactType1>(() => fact11, f => f.TestProperty.StartsWith("Valid"))
-                .Match<FactType2>(() => fact21, f => f.TestProperty.StartsWith("Valid"), f => f.JoinProperty == fact11.TestProperty);
+                .Match<FactType1>(() => fact1, f => f.TestProperty.StartsWith("Valid"))
+                .Match<FactType2>(() => fact2, f => f.TestProperty.StartsWith("Valid"), f => f.JoinProperty == fact1.TestProperty)
+                .Not<FactType3>(f => f.TestProperty.StartsWith("Invalid"))
+                .Exists<FactType3>(f => f.TestProperty.StartsWith("Valid"))
+                .Query(() => group, q => q
+                    .Match<FactType5>()
+                    .Where(f => f.TestProperty.StartsWith("Valid"))
+                    .GroupBy(f => f.TestProperty)
+                    .SelectMany(x => x)
+                    .Collect()
+                    .Where(c => c.Any()));
 
             Then()
                 .Do(ctx => Action());
