723e0f1c3ba776b7e73108324214c6660ebc85f8
snikolayev <snikolayev@github.com>
2017-03-30 22:16:29 -0400

    Update assembly description

--- src/NRules/NRules/Properties/AssemblyInfo.cs
+++ src/NRules/NRules/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Security;
 
 [assembly: AssemblyTitle("NRules")]
-[assembly: AssemblyDescription("Business rules engine for .NET")]
+[assembly: AssemblyDescription("Rules engine for .NET")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: AllowPartiallyTrustedCallers]
