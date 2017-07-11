25361e3a8e4d0963c4f5208e0b19545a71a01b4b
snikolayev <snikolayev@github.com>
2017-07-11 00:26:42 -0400

    Node sharing for calculated values
    - Add tests
    - Fix node sharing
    - Fix snapshot creation

--- src/NRules/Tests/NRules.IntegrationTests/TwoFactCalculateRuleTest.cs
+++ src/NRules/Tests/NRules.IntegrationTests/TwoFactCalculateRuleTest.cs
@@ -44,6 +44,34 @@ namespace NRules.IntegrationTests
         }
 
         [Fact]
+        public void Fire_OneMatchingFactOfEachKindFireThenSecondFactUpdated_FiresTwice()
+        {
+            //Arrange - 1
+            var fact1 = new FactType1 { TestProperty = "Valid Value 1" };
+            var fact2 = new FactType2 { TestProperty = "Valid Value 2", JoinProperty = "Valid Value 1" };
+            Session.Insert(fact1);
+            Session.Insert(fact2);
+
+            //Act - 1
+            Session.Fire();
+
+            //Assert - 1
+            AssertFiredOnce();
+            Assert.Equal("Valid Value 1|Valid Value 2", GetFiredFact<CalculatedFact>(0).Value);
+
+            //Arrange - 2
+            fact2.TestProperty = "Valid Value 22";
+            Session.Update(fact2);
+
+            //Act - 2
+            Session.Fire();
+
+            //Assert - 2
+            AssertFiredTwice();
+            Assert.Equal("Valid Value 1|Valid Value 22", GetFiredFact<CalculatedFact>(1).Value);
+        }
+
+        [Fact]
         public void Fire_OneMatchingFactOfEachKindSecondFactRetracted_DoesNotFire()
         {
             //Arrange
@@ -59,6 +87,26 @@ namespace NRules.IntegrationTests
 
             //Assert
             AssertDidNotFire();
+        }
+
+        [Fact]
+        public void Fire_TwoMatchingSetsOfFacts_FiresTwiceCalculatesPerSet()
+        {
+            //Arrange
+            var fact11 = new FactType1 { TestProperty = "Valid Value 11" };
+            var fact21 = new FactType2 { TestProperty = "Valid Value 21", JoinProperty = "Valid Value 11" };
+            var fact12 = new FactType1 { TestProperty = "Valid Value 12" };
+            var fact22 = new FactType2 { TestProperty = "Valid Value 22", JoinProperty = "Valid Value 12" };
+            Session.InsertAll(new []{fact11, fact12});
+            Session.InsertAll(new []{fact21, fact22});
+
+            //Act
+            Session.Fire();
+
+            //Assert
+            AssertFiredTwice();
+            Assert.Equal("Valid Value 11|Valid Value 21", GetFiredFact<CalculatedFact>(0).Value);
+            Assert.Equal("Valid Value 12|Valid Value 22", GetFiredFact<CalculatedFact>(1).Value);
         }
 
         protected override void SetUpRules()
