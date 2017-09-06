03755528665d734100d3d8cbf2a8b78cef5b0a9b
snikolayev <snikolayev@github.com>
2017-09-05 21:40:00 -0400

    Tests for predicate filter

--- src/NRules/Tests/NRules.IntegrationTests/TwoFactFilterRuleTest.cs
+++ src/NRules/Tests/NRules.IntegrationTests/TwoFactFilterRuleTest.cs
@@ -10,8 +10,8 @@ namespace NRules.IntegrationTests
         public void Fire_MatchingFacts_Fires()
         {
             //Arrange
-            var fact1 = new FactType1 { TestProperty = "Valid Value 1" };
-            var fact2 = new FactType2 { TestProperty = "Valid Value 2" };
+            var fact1 = new FactType1 {TestProperty = "Valid Value 1", Allow = true};
+            var fact2 = new FactType2 {TestProperty = "Valid Value 2", Allow = true};
 
             Session.Insert(fact1);
             Session.Insert(fact2);
@@ -27,8 +27,8 @@ namespace NRules.IntegrationTests
         public void Fire_MatchingFactsInsertAndFireThenUpdateKey1ChangedAndFire_FiresTwice()
         {
             //Arrange
-            var fact1 = new FactType1 { TestProperty = "Valid Value 1" };
-            var fact2 = new FactType2 { TestProperty = "Valid Value 2" };
+            var fact1 = new FactType1 {TestProperty = "Valid Value 1", Allow = true};
+            var fact2 = new FactType2 {TestProperty = "Valid Value 2", Allow = true};
 
             Session.Insert(fact1);
             Session.Insert(fact2);
@@ -48,8 +48,8 @@ namespace NRules.IntegrationTests
         public void Fire_MatchingFactsInsertAndFireThenUpdateKey2ChangedAndFire_FiresTwice()
         {
             //Arrange
-            var fact1 = new FactType1 { TestProperty = "Valid Value 1" };
-            var fact2 = new FactType2 { TestProperty = "Valid Value 2" };
+            var fact1 = new FactType1 {TestProperty = "Valid Value 1", Allow = true};
+            var fact2 = new FactType2 {TestProperty = "Valid Value 2", Allow = true};
 
             Session.Insert(fact1);
             Session.Insert(fact2);
@@ -69,8 +69,8 @@ namespace NRules.IntegrationTests
         public void Fire_MatchingFactsInsertAndFireThenUpdateKeyDidNotChangeAndFire_FiresOnce()
         {
             //Arrange
-            var fact1 = new FactType1 { TestProperty = "Valid Value 1" };
-            var fact2 = new FactType2 { TestProperty = "Valid Value 2" };
+            var fact1 = new FactType1 {TestProperty = "Valid Value 1", Allow = true};
+            var fact2 = new FactType2 {TestProperty = "Valid Value 2", Allow = true};
 
             Session.Insert(fact1);
             Session.Insert(fact2);
@@ -85,6 +85,57 @@ namespace NRules.IntegrationTests
             AssertFiredOnce();
         }
 
+        [Fact]
+        public void Fire_MatchingFactsOnePredicateDoesNotAllow_DoesNotFire()
+        {
+            //Arrange
+            var fact1 = new FactType1 {TestProperty = "Valid Value 1", Allow = false};
+            var fact2 = new FactType2 {TestProperty = "Valid Value 2", Allow = true};
+
+            Session.Insert(fact1);
+            Session.Insert(fact2);
+
+            //Act
+            Session.Fire();
+
+            //Assert
+            AssertDidNotFire();
+        }
+
+        [Fact]
+        public void Fire_MatchingFactsSecondPredicateDoesNotAllow_DoesNotFire()
+        {
+            //Arrange
+            var fact1 = new FactType1 {TestProperty = "Valid Value 1", Allow = true};
+            var fact2 = new FactType2 {TestProperty = "Valid Value 2", Allow = false};
+
+            Session.Insert(fact1);
+            Session.Insert(fact2);
+
+            //Act
+            Session.Fire();
+
+            //Assert
+            AssertDidNotFire();
+        }
+
+        [Fact]
+        public void Fire_MatchingFactsBothPredicatesDoNotAllow_DoesNotFire()
+        {
+            //Arrange
+            var fact1 = new FactType1 {TestProperty = "Valid Value 1", Allow = false};
+            var fact2 = new FactType2 {TestProperty = "Valid Value 2", Allow = false};
+
+            Session.Insert(fact1);
+            Session.Insert(fact2);
+
+            //Act
+            Session.Fire();
+
+            //Assert
+            AssertDidNotFire();
+        }
+
         protected override void SetUpRules()
         {
             SetUpRule<TestRule>();
@@ -93,11 +144,13 @@ namespace NRules.IntegrationTests
         public class FactType1
         {
             public string TestProperty { get; set; }
+            public bool Allow { get; set; }
         }
 
         public class FactType2
         {
             public string TestProperty { get; set; }
+            public bool Allow { get; set; }
         }
 
         public class TestRule : Rule
@@ -112,7 +165,8 @@ namespace NRules.IntegrationTests
                     .Match<FactType2>(() => fact2, f => f.TestProperty.StartsWith("Valid"));
 
                 Filter()
-                    .OnChange(() => fact1.TestProperty, () => fact2.TestProperty);
+                    .OnChange(() => fact1.TestProperty, () => fact2.TestProperty)
+                    .Where(() => fact1.Allow, () => fact2.Allow);
 
                 Then()
                     .Do(ctx => ctx.NoOp());
