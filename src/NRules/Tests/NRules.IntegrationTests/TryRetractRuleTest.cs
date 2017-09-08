ae8dba398d0894a6b4ca1a9ea59c51d76fa094f8
Sergiy Nikolayev <sergiy_nikolayev@hotmail.com>
2017-09-08 03:54:00 +0300

    Merge pull request #131 from Krizzzn/develop
    Added failing test for possible regression.

---
+++ src/NRules/Tests/NRules.IntegrationTests/TryRetractRuleTest.cs
@@ -0,0 +1,81 @@
+﻿using NRules.Fluent.Dsl;
+using NRules.IntegrationTests.TestAssets;
+using Xunit;
+using System.Linq;
+
+namespace NRules.IntegrationTests
+{
+    public class TryRetractRuleTest : BaseRuleTestFixture
+    {
+        [Fact]
+        public void Fire_OneMatchingFact_FiresOnceAndRetractsFact()
+        {
+            //Arrange
+            var fact1 = new FactType { TestProperty = "Valid Value 1" };
+            var facts = new[] { fact1 };
+            Session.InsertAll(facts);
+
+            //Act
+            Session.Fire();
+
+            //Assert
+            AssertFiredOnce();
+            Assert.Equal(0, Session.Query<FactType>().Count());
+        }
+
+        [Fact]
+        public void Fire_NoMatchingFact_DoesNotFire()
+        {
+            //Arrange
+            var fact1 = new FactType { TestProperty = "Invalid Value 1" };
+            var facts = new[] { fact1 };
+            Session.InsertAll(facts);
+
+            //Act
+            Session.Fire();
+
+            //Assert
+            AssertDidNotFire();
+        }
+
+        [Fact]
+        public void Fire_TwoMatchingFacts_FiresTwiceAndRetractsFacts()
+        {
+            //Arrange
+            var fact1 = new FactType { TestProperty = "Valid Value 1" };
+            var fact2 = new FactType { TestProperty = "Valid Value 2" };
+            var facts = new[] { fact1, fact2 };
+            Session.InsertAll(facts);
+
+            //Act
+            Session.Fire();
+
+            //Assert
+            AssertFiredTwice();
+            Assert.Equal(0, Session.Query<FactType>().Count());
+        }
+
+        protected override void SetUpRules()
+        {
+            SetUpRule<TestRule>();
+        }
+
+        public class FactType
+        {
+            public string TestProperty { get; set; }
+        }
+
+        public class TestRule : Rule
+        {
+            public override void Define()
+            {
+                FactType fact = null;
+
+                When()
+                    .Match<FactType>(() => fact, f => f.TestProperty.StartsWith("Valid"));
+                Then()
+                    .Do(ctx => ctx.TryRetract(fact));
+            }
+        }
+    }
+}
