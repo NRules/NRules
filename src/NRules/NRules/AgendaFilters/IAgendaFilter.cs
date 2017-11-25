59786198bcd34f61c4f78ce75998c08b9e36a86f
snikolayev <snikolayev@github.com>
2017-11-25 13:54:30 -0500

    Cleanup XML documentation

--- src/NRules/NRules/AgendaFilters/IAgendaFilter.cs
+++ src/NRules/NRules/AgendaFilters/IAgendaFilter.cs
@@ -1,9 +1,13 @@
 ﻿namespace NRules.AgendaFilters
 {
     /// <summary>
-    /// Filters rule matches (activations) before they are added to the agenda.
+    /// Agenda filters are applied to rule matches (activations) before they are added to the agenda.
     /// If activation does not pass all the filters, it is not added to the agenda, and so the rule will not fire.
     /// </summary>
+    /// <remarks>
+    /// Agenda filters never remove activations from the agenda. If an activation passed all filters and was added to the agenda,
+    /// and then later was modified and failed to pass the filters, it still remains in the agenda.
+    /// </remarks>
     public interface IAgendaFilter  {
         /// <summary>
         /// Tests rule activation whether it should be added to the agenda.
