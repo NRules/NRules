7803272d7160ee703e3abf2c52a5565423d23fba
snikolayev <snikolayev@github.com>
2017-11-28 23:15:42 -0500

    Sample with rule filter

--- samples/SimpleRules/SimpleRules/Domain/Order.cs
+++ samples/SimpleRules/SimpleRules/Domain/Order.cs
@@ -10,10 +10,7 @@
         public bool IsOpen { get; private set; }
         public bool IsDiscounted { get { return PercentDiscount > 0; } }
 
-        public double Amount
-        {
-            get { return UnitPrice*Quantity*(1.0 - PercentDiscount/100.0); }
-        }
+        public double Amount { get; set; }
 
         public Order(int id, Customer customer, int quantity, double unitPrice)
         {
