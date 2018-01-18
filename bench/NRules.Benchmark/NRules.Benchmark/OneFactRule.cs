cc982bc3bd21e03aafbaebe7203d648a946f2be3
snikolayev <snikolayev@github.com>
2018-01-18 00:15:39 -0500

    First take on performance benchmarks

---
+++ bench/NRules.Benchmark/NRules.Benchmark/OneFactRule.cs
@@ -0,0 +1,79 @@
+﻿using BenchmarkDotNet.Attributes;
+using NRules.Fluent;
+using NRules.Fluent.Dsl;
+
+namespace NRules.Benchmark
+{
+    public class OneFactRule
+    {
+        private ISessionFactory _factory;
+        private TestFact[] _facts;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var repository = new RuleRepository();
+            repository.Load(x => x.NestedTypes().PrivateTypes().From(xx => xx.Type(typeof(TestRule))));
+
+            _factory = repository.Compile();
+
+            _facts = new TestFact[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                _facts[i] = new TestFact{Test = $"Valid {i}"};
+            }
+        }
+
+        [Params(10, 100, 1000, 10000)]
+        public int Count { get; set; }
+
+        [Benchmark]
+        public int Insert()
+        {
+            var session = _factory.CreateSession();
+            session.InsertAll(_facts);
+            return session.Fire();
+        }
+
+        [Benchmark]
+        public int InsertUpdate()
+        {
+            var session = _factory.CreateSession();
+            session.InsertAll(_facts);
+            session.UpdateAll(_facts);
+            return session.Fire();
+        }
+
+        [Benchmark]
+        public int InsertRetract()
+        {
+            var session = _factory.CreateSession();
+            session.InsertAll(_facts);
+            session.RetractAll(_facts);
+            return session.Fire();
+        }
+
+        private class TestFact
+        {
+            public string Test { get; set; }
+        }
+
+        private class TestRule : Rule
+        {
+            public override void Define()
+            {
+                TestFact fact = null;
+
+                When()
+                    .Match(() => fact, x => x.Test.StartsWith("Valid"));
+
+                Then()
+                    .Do(_ => Nothing());
+            }
+
+            private void Nothing()
+            {
+            }
+        }
+    }
+}
---
+++ bench/NRules.Benchmark/NRules.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace NRules.Benchmark
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<OneFactRule>();
+        }
+    }
+}
