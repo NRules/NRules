5a0caa520fe8ac7fee724ce6ca40410ab6f28457
snikolayev <snikolayev@github.com>
2018-01-20 22:29:52 -0500

    Target .net 4.6.2 for unit/integration tests

--- default.ps1
+++ default.ps1
@@ -111,7 +111,7 @@ task Test -depends Compile -precondition { return $component.ContainsKey('test')
     $projects = Get-DotNetProjects $tests_dir
     foreach ($project in $projects) {
         Push-Location $project
-        exec { dotnet test --no-build --configuration $configuration --framework net46 --verbosity minimal --logger "trx;LogFileName=TestResult.trx" }
+        exec { dotnet test --no-build --configuration $configuration --framework net462 --verbosity minimal --logger "trx;LogFileName=TestResult.trx" }
         Pop-Location
     }
     
