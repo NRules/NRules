5bf161970f760b9a8a8d44d9e8a1329a151640cf
Linan Qiu <linanqiu@gmail.com>
2018-05-27 18:36:22 -0400

    Double reference from query tests

--- src/NRules/Tests/NRules.IntegrationTests/FromQueryComplexReferenceTest.cs
+++ src/NRules/Tests/NRules.IntegrationTests/FromQueryComplexReferenceTest.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NRules.Fluent.Dsl;
 using NRules.IntegrationTests.TestAssets;
 using Xunit;
