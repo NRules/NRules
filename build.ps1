b235b3ab4fec8a7fbe75acf0935c71315c223077
snikolayev <snikolayev@github.com>
2019-05-30 06:36:51 -0400

    Upgrade tests and benchmarks to .net 4.7.2

--- build.ps1
+++ build.ps1
@@ -20,7 +20,7 @@ $components = @{
         }
         test = @{
             location = 'Tests'
-            frameworks = @('net462', 'netcoreapp1.0', 'netcoreapp2.0')
+            frameworks = @('net472', 'netcoreapp1.0', 'netcoreapp2.0')
         }
         bin = @{
             frameworks = @('net45', 'netstandard1.0', 'netstandard2.0')
@@ -136,15 +136,15 @@ $components = @{
             tool = 'dotnet'
         }
         bin = @{
-            frameworks = @('net462')
-            'net462' = @{
+            frameworks = @('net472')
+            'net472' = @{
                 include = @(
-                    "NRules.Benchmark\bin\$configuration\net462"
+                    "NRules.Benchmark\bin\$configuration\net472"
                 )
             }
         }
         run = @{
-            exe = @('net462\NRules.Benchmark.exe')
+            exe = @('net472\NRules.Benchmark.exe')
         }
     };
     'Documentation' = @{
