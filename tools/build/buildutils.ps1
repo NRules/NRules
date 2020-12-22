55c198add13315eb90b9a481ecd328eb31a2ae26
Sergiy Nikolayev <snikolayev@github.com>
2020-12-21 22:48:41 -0500

    Revert "Remove msbuild from build tooling"
    This reverts commit e892856d91cacca8bbc77b70beaf2e29a735698d.

--- tools/build/buildutils.ps1
+++ tools/build/buildutils.ps1
@@ -12,6 +12,14 @@ function Create-Directory($directoryName) {
     New-Item $directoryName -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 }
 
+function Get-RegistryValues($key) {
+    (Get-Item $key -ErrorAction SilentlyContinue).GetValueNames()
+}
+
+function Get-RegistryValue($key, $value) {
+    (Get-ItemProperty $key $value -ErrorAction SilentlyContinue).$value
+}
+
 function Update-InternalsVisible([string] $path, [string] $publicKey, [string] $assemblyInfoFileName = "AssemblyInfo.cs") {
     Write-Host Patching AssemblyInfo files with public key
     
--- tools/build/default.ps1
+++ tools/build/default.ps1
@@ -32,6 +32,10 @@ task Init {
     $script:packagesDir = "$baseDir\packages"
     $script:toolsDir = "$baseDir\tools"
     
+    $frameworkRoot = Get-RegistryValue 'HKLM:\SOFTWARE\Microsoft\.NETFramework\' 'InstallRoot' 
+    $frameworkRoot = $frameworkRoot + "v4.0.30319"
+    $script:msbuild = $frameworkRoot + "\msbuild.exe"
+    
     Install-DotNetCli $toolsDir\.dotnet $sdkVersion
     Install-NuGet $toolsDir\.nuget $nugetVersion
 }
