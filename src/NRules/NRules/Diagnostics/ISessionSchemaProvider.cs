39090dc9dd226bb8cf12970a420d0f48a143fb0f
Sergiy Nikolayev <snikolayev@github.com>
2021-03-08 00:18:18 -0500

    Cleanup XML docs

--- src/NRules/NRules/Diagnostics/ISessionSchemaProvider.cs
+++ src/NRules/NRules/Diagnostics/ISessionSchemaProvider.cs
@@ -1,14 +1,14 @@
 ﻿namespace NRules.Diagnostics
 {
     /// <summary>
-    /// Provides the rules schema in a form of a Rete network graph,for diagnostics.
+    /// Provides the rules schema in a form of a Rete network graph, for diagnostics.
     /// </summary>
     public interface ISessionSchemaProvider
     {
         /// <summary>
         /// Returns the rules schema as a graph representing the structure of the underlying Rete network.
         /// </summary>
-        /// <returns>Session schema.</returns>
+        /// <returns>Session schema as a Rete graph.</returns>
         ReteGraph GetSchema();
     }
 }
