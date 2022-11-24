b29ced847653f692c3989b55b81765a78e170684
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-11-23 22:23:11 -0500

    Refactor ExpressionComparer in tests

--- src/NRules/Tests/NRules.Json.Tests/Utilities/ExpressionComparer.cs
+++ src/NRules/Tests/NRules.Json.Tests/Utilities/ExpressionComparer.cs
@@ -8,90 +8,116 @@ namespace NRules.Json.Tests.Utilities;
 
 public static class ExpressionComparer
 {
-    public static bool AreEqual(Expression x, Expression y)
-    {
-        return ExpressionEqual(x, y, null, null);
-    }
+    public static bool AreEqual(Expression x, Expression y) => AreEqual(x, y, null, null);
 
-    private static bool ExpressionEqual(Expression x, Expression y, LambdaExpression rootX, LambdaExpression rootY)
+    private static bool AreEqual(Expression x, Expression y, LambdaExpression rootX, LambdaExpression rootY)
     {
         if (ReferenceEquals(x, y))
             return true;
-        if (x == null || y == null)
+        if (x is null || y is null)
             return false;
         if (x.NodeType != y.NodeType || x.Type != y.Type)
             return false;
 
-        return (x, y) switch
+        switch ((x, y))
         {
-            (LambdaExpression lx, LambdaExpression ly) => CollectionsEqual(lx.Parameters, ly.Parameters, lx, ly)
-                && ExpressionEqual(lx.Body, ly.Body, lx, ly),
-            (MemberExpression mex, MemberExpression mey) => MemberExpressionsEqual(mex, mey, rootX, rootY),
-            (BinaryExpression bx, BinaryExpression by) => bx.Method == @by.Method
-                && ExpressionEqual(bx.Left, @by.Left, rootX, rootY)
-                && ExpressionEqual(bx.Right, @by.Right, rootX, rootY),
-            (UnaryExpression ux, UnaryExpression uy) => ux.Method == uy.Method
-                && ExpressionEqual(ux.Operand, uy.Operand, rootX, rootY),
-            (ParameterExpression px, ParameterExpression py) => rootX.Parameters.IndexOf(px) == rootY.Parameters.IndexOf(py)
-                && px.Name == py.Name,
-            (MethodCallExpression cx, MethodCallExpression cy) => cx.Method == cy.Method
-                && ExpressionEqual(cx.Object, cy.Object, rootX, rootY)
-                && CollectionsEqual(cx.Arguments, cy.Arguments, rootX, rootY),
-            (InvocationExpression ix, InvocationExpression iy) => ExpressionEqual(ix.Expression, iy.Expression, rootX, rootY)
-                && CollectionsEqual(ix.Arguments, iy.Arguments, rootX, rootY),
-            (NewExpression nx, NewExpression ny) => nx.Constructor == ny.Constructor
-                && CollectionsEqual(nx.Arguments, ny.Arguments, rootX, rootY),
-            (MemberInitExpression mix, MemberInitExpression miy) => ExpressionEqual(mix.NewExpression, miy.NewExpression, rootX, rootY)
-                && MemberBindingCollectionsEqual(mix.Bindings, miy.Bindings, rootX, rootY),
-            (ListInitExpression lix, ListInitExpression liy) => ExpressionEqual(lix.NewExpression, liy.NewExpression, rootX, rootY)
-                && ElementInitCollectionsEqual(lix.Initializers, liy.Initializers, rootX, rootY),
-            (ConstantExpression cx, ConstantExpression cy) => Equals(cx.Value, cy.Value),
-            (TypeBinaryExpression tbx, TypeBinaryExpression tby) => tbx.TypeOperand == tby.TypeOperand,
-            (ConditionalExpression cx, ConditionalExpression cy) => ExpressionEqual(cx.Test, cy.Test, rootX, rootY)
-                && ExpressionEqual(cx.IfTrue, cy.IfTrue, rootX, rootY)
-                && ExpressionEqual(cx.IfFalse, cy.IfFalse, rootX, rootY),
-            (NewArrayExpression ax, NewArrayExpression ay) => CollectionsEqual(ax.Expressions, ay.Expressions, rootX, rootY),
-            (DefaultExpression, DefaultExpression) => true,
-            (BlockExpression bx, BlockExpression by) => ExpressionEqual(bx.Result, by.Result, rootX, rootY)
-                && CollectionsEqual(bx.Expressions, by.Expressions, rootX, rootY),
-            _ => throw new NotImplementedException(x.ToString()),
-        };
+            case (LambdaExpression lx, LambdaExpression ly):
+                return AreEqual(lx.Parameters, ly.Parameters, lx, ly)
+                    && AreEqual(lx.Body, ly.Body, lx, ly);
+            case (MemberExpression mex, MemberExpression mey):
+                return AreEqual(mex, mey, rootX, rootY);
+            case (BinaryExpression bx, BinaryExpression by):
+                return bx.Method == @by.Method
+                    && AreEqual(bx.Left, @by.Left, rootX, rootY)
+                    && AreEqual(bx.Right, @by.Right, rootX, rootY);
+            case (UnaryExpression ux, UnaryExpression uy):
+                return ux.Method == uy.Method
+                    && AreEqual(ux.Operand, uy.Operand, rootX, rootY);
+            case (ParameterExpression px, ParameterExpression py):
+                return rootX.Parameters.IndexOf(px) == rootY.Parameters.IndexOf(py)
+                    && px.Name == py.Name;
+            case (MethodCallExpression cx, MethodCallExpression cy):
+                return cx.Method == cy.Method
+                    && AreEqual(cx.Object, cy.Object, rootX, rootY)
+                    && AreEqual(cx.Arguments, cy.Arguments, rootX, rootY);
+            case (InvocationExpression ix, InvocationExpression iy):
+                return AreEqual(ix.Expression, iy.Expression, rootX, rootY)
+                    && AreEqual(ix.Arguments, iy.Arguments, rootX, rootY);
+            case (NewExpression nx, NewExpression ny):
+                return nx.Constructor == ny.Constructor
+                    && AreEqual(nx.Arguments, ny.Arguments, rootX, rootY);
+            case (MemberInitExpression mix, MemberInitExpression miy):
+                return AreEqual(mix.NewExpression, miy.NewExpression, rootX, rootY)
+                    && AreEqual(mix.Bindings, miy.Bindings, rootX, rootY);
+            case (ListInitExpression lix, ListInitExpression liy):
+                return AreEqual(lix.NewExpression, liy.NewExpression, rootX, rootY)
+                    && AreEqual(lix.Initializers, liy.Initializers, rootX, rootY);
+            case (ConstantExpression cx, ConstantExpression cy):
+                return Equals(cx.Value, cy.Value);
+            case (TypeBinaryExpression tbx, TypeBinaryExpression tby):
+                return tbx.TypeOperand == tby.TypeOperand;
+            case (ConditionalExpression cx, ConditionalExpression cy):
+                return AreEqual(cx.Test, cy.Test, rootX, rootY)
+                    && AreEqual(cx.IfTrue, cy.IfTrue, rootX, rootY)
+                    && AreEqual(cx.IfFalse, cy.IfFalse, rootX, rootY);
+            case (NewArrayExpression ax, NewArrayExpression ay):
+                return AreEqual(ax.Expressions, ay.Expressions, rootX, rootY);
+            case (DefaultExpression, DefaultExpression):
+                return true;
+            case (BlockExpression bx, BlockExpression by):
+                return AreEqual(bx.Result, by.Result, rootX, rootY)
+                    && AreEqual(bx.Expressions, by.Expressions, rootX, rootY);
+            default:
+                throw new NotImplementedException(x.ToString());
+        }
     }
 
-    private static bool MemberBindingCollectionsEqual(IReadOnlyCollection<MemberBinding> x, IReadOnlyCollection<MemberBinding> y, LambdaExpression rootX, LambdaExpression rootY) =>
-        x.Count == y.Count
-            && x.Zip(y, (first, second) => new { X = first, Y = second })
-                .All(o => MemberBindingsEqual(o.X, o.Y, rootX, rootY));
+    private static bool AreEqual(IReadOnlyCollection<MemberBinding> x, IReadOnlyCollection<MemberBinding> y, LambdaExpression rootX, LambdaExpression rootY)
+    {
+        return x.Count == y.Count
+            && x.Zip(y, (first, second) => (first, second))
+                .All(o => AreEqual(o.first, o.second, rootX, rootY));
+    }
 
-    private static bool ElementInitCollectionsEqual(IReadOnlyCollection<ElementInit> x, IReadOnlyCollection<ElementInit> y, LambdaExpression rootX, LambdaExpression rootY) =>
-        x.Count == y.Count
-            && x.Zip(y, (first, second) => new { X = first, Y = second })
-                .All(o => ElementInitsEqual(o.X, o.Y, rootX, rootY));
+    private static bool AreEqual(IReadOnlyCollection<ElementInit> x, IReadOnlyCollection<ElementInit> y, LambdaExpression rootX, LambdaExpression rootY)
+    {
+        return x.Count == y.Count
+            && x.Zip(y, (first, second) => (first, second))
+                .All(o => AreEqual(o.first, o.second, rootX, rootY));
+    }
 
-    private static bool CollectionsEqual(IReadOnlyCollection<Expression> x, IReadOnlyCollection<Expression> y, LambdaExpression rootX, LambdaExpression rootY) =>
-        x.Count == y.Count
-            && x.Zip(y, (first, second) => new { X = first, Y = second })
-                .All(o => ExpressionEqual(o.X, o.Y, rootX, rootY));
+    private static bool AreEqual(IReadOnlyCollection<Expression> x, IReadOnlyCollection<Expression> y, LambdaExpression rootX, LambdaExpression rootY)
+    {
+        return x.Count == y.Count
+            && x.Zip(y, (first, second) => (first, second))
+                .All(o => AreEqual(o.first, o.second, rootX, rootY));
+    }
 
-    private static bool ElementInitsEqual(ElementInit x, ElementInit y, LambdaExpression rootX, LambdaExpression rootY) =>
-        Equals(x.AddMethod, y.AddMethod)
-            && CollectionsEqual(x.Arguments, y.Arguments, rootX, rootY);
+    private static bool AreEqual(ElementInit x, ElementInit y, LambdaExpression rootX, LambdaExpression rootY)
+    {
+        return Equals(x.AddMethod, y.AddMethod)
+            && AreEqual(x.Arguments, y.Arguments, rootX, rootY);
+    }
 
-    private static bool MemberBindingsEqual(MemberBinding x, MemberBinding y, LambdaExpression rootX, LambdaExpression rootY)
+    private static bool AreEqual(MemberBinding x, MemberBinding y, LambdaExpression rootX, LambdaExpression rootY)
     {
         if (x.BindingType != y.BindingType)
             return false;
 
-        return (x, y) switch
+        switch ((x, y))
         {
-            (MemberAssignment max, MemberAssignment may) => ExpressionEqual(max.Expression, may.Expression, rootX, rootY),
-            (MemberMemberBinding mmbx, MemberMemberBinding mmby) => MemberBindingCollectionsEqual(mmbx.Bindings, mmby.Bindings, rootX, rootY),
-            (MemberListBinding mlbx, MemberListBinding mlby) => ElementInitCollectionsEqual(mlbx.Initializers, mlby.Initializers, rootX, rootY),
-            _ => throw new ArgumentOutOfRangeException($"Unsupported binding type. BindingType={x.BindingType}"),
-        };
+            case (MemberAssignment max, MemberAssignment may):
+                return AreEqual(max.Expression, may.Expression, rootX, rootY);
+            case (MemberMemberBinding mmbx, MemberMemberBinding mmby):
+                return AreEqual(mmbx.Bindings, mmby.Bindings, rootX, rootY);
+            case (MemberListBinding mlbx, MemberListBinding mlby):
+                return AreEqual(mlbx.Initializers, mlby.Initializers, rootX, rootY);
+            default:
+                throw new ArgumentOutOfRangeException($"Unsupported binding type. BindingType={x.BindingType}");
+        }
     }
 
-    private static bool MemberExpressionsEqual(MemberExpression x, MemberExpression y, LambdaExpression rootX, LambdaExpression rootY)
+    private static bool AreEqual(MemberExpression x, MemberExpression y, LambdaExpression rootX, LambdaExpression rootY)
     {
         if (x.Expression == null || y.Expression == null)
             return Equals(x.Member, y.Member);
@@ -118,20 +144,25 @@ public static class ExpressionComparer
             case ExpressionType.Modulo:
             case ExpressionType.Power:
             case ExpressionType.Conditional:
-                return Equals(x.Member, y.Member) && ExpressionEqual(x.Expression, y.Expression, rootX, rootY);
+                return Equals(x.Member, y.Member) && AreEqual(x.Expression, y.Expression, rootX, rootY);
             case ExpressionType.New:
             case ExpressionType.Call:
-                return ExpressionEqual(x.Expression, y.Expression, rootX, rootY);
+                return AreEqual(x.Expression, y.Expression, rootX, rootY);
             default:
                 throw new NotImplementedException(x.ToString());
         }
     }
 
-    private static object GetValueOfConstantExpression(MemberExpression mex) =>
-        mex switch
+    private static object GetValueOfConstantExpression(MemberExpression mex)
+    {
+        switch (mex)
         {
-            { Expression: ConstantExpression ce, Member: FieldInfo fi } => fi.GetValue(ce.Value),
-            { Expression: ConstantExpression ce, Member: PropertyInfo pi } => pi.GetValue(ce.Value, null),
-            _ => throw new ArgumentException($"Unsupported member type. MemberType={mex.Member.GetType()}")
-        };
+            case { Expression: ConstantExpression ce, Member: FieldInfo fi }:
+                return fi.GetValue(ce.Value);
+            case { Expression: ConstantExpression ce, Member: PropertyInfo pi }:
+                return pi.GetValue(ce.Value, null);
+            default:
+                throw new ArgumentException($"Unsupported member type. MemberType={mex.Member.GetType()}");
+        }
+    }
 }
