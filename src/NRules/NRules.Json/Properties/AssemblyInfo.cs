a1e8a8e137d0ce41d7af6e0ffc08c5f2095346d1
Sergiy Nikolayev <snikolayev@github.com>
2022-11-24 15:35:15 -0500

    Add missing assembly attributes to json and testing libraries

---
+++ src/NRules/NRules.Json/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NRules.Json")]
+[assembly: AssemblyDescription("Json serialization for NRules")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
